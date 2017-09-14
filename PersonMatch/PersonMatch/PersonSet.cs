ec609831f66612c0db6ca88dbddf389c8d364f81
Jason Bingham <jbingham408@gmail.com>
2017-09-14 14:37:22 -0600

    Changed some implentations of exporter, importer, and ComparePairs. Also renamed the files to PersonMatch

---
+++ PersonMatch/PersonMatch/PersonSet.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonMatcher
+{
+    public class PersonSet : List<Person>
+    {
+        public Importer myImporter { get; set; }
+        public Exporter myExporter { get; set; }
+        public string myImportFilename { get; set; }
+        public string myExportFilename { get; set; }
+        public ComparePairs myCompareStrategy { get; set; }
+        private Match myMatches { get; set; }
+
+        public void ReadInFile()
+        {
+            myImporter.Read(this, myImportFilename);
+        }
+
+        public void GetMatches()
+        {
+            myMatches = new Match();
+            myMatches.FindMatches(this, myCompareStrategy);
+        }
+
+        public void WriteMatches()
+        {
+            if (myExportFilename == null)
+                WriteMatchesToScreen();
+            else
+                WriteMatchesToFile();
+        }
+
+        public void WriteMatchesToFile()
+        {
+            myExporter.Write(myMatches.GetMatchesList(), myExportFilename);
+        }
+
+        public void WriteMatchesToScreen()
+        {
+            myExporter = new ScreenExporter() { list = this };
+            myExporter.Write(myMatches.GetMatchesList());
+        }
+
+    }
+}
---
+++ PersonMatch/PersonMatchUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PersonMatchUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PersonMatchUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3f18edb6-f97f-4bbd-916a-67fa470d24e4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
